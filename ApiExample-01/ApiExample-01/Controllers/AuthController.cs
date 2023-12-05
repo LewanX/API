979e1411993b00965da59d2253ab66c89c1906e1
Alan Jason Lewandowski <lewandowski301301@gmail.com>
2023-12-05 18:43:32 -0300

    Initial Commit

---
+++ ApiExample-01/ApiExample-01/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using ApiExample_01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExample_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        public static List<User> users = new List<User>();
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+           
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult <User>Login(UserDto request )
+        {
+            if (user.Username!=request.Username)
+            {
+                return BadRequest("User Not Found!");
+            }
+            if ()
+            {
+
+            }
+
+
+            return Ok();
+        }
+
+
+    }
+}
